c8dfaf63ed2b54462525e9c8b38b3ae457bf2c78
Iacopo Poli <iacopo.poli93@gmail.com>
2015-05-06 19:07:17 +0200

    Porject ended

--- irrevocable.py
+++ irrevocable.py
@@ -1,3 +1,21 @@
+'''This program simulates Irrevocable model described in "Mathematical modeling of 
+	allegiance to political parties", Iacopo Poli, 2015
+	
+    Copyright (C) 2015  Iacopo Poli
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
+    
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
--- paperlib.py
+++ paperlib.py
@@ -1,20 +1,3 @@
-'''Functions used in other programs for non linear dynamics simulation 
-    Copyright (C) 2015  Iacopo Poli
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
-
 import random as rnd
 
 #randomic takes population, density of population per squared kilometer and area of influence in squared kilometers
@@ -26,3 +9,14 @@ def randomic(pop, avg, area):
     
     rand_a = (avg*area/float(pop))*deviation
     return rand_a
+
+# sum_vector takes a vector and return the sum of his elements
+
+def sum_vector(vector, length):
+
+	sum = 0
+	
+	for n in range(0, length):
+		sum = sum + vector[n]
+	
+	return sum
